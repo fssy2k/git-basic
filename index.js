5018eb100331c6733fbcb99b42a9b2b18e18f4bc
fssy2k <fssy2k@nate.com>
2024-01-19 15:29:59 +0900

    feat login function update

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 /* 
     index.html 에서 사용되는 js
 */
-function login(username, password) {
+function login(username, password, flag) {
     //login 처리
+    if(username != null && flag) {        
+        console.log(username);
+        console.log(flag);
+    }
     return true;
 };
 
